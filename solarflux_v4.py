67e2e66e8f7b31cfc59517330c4531419df8bea4
quarkz24 <josephinehedley@outlook.com>
2023-10-10 09:26:36 +0100

    Update solarflux_v4.py
    changed latitude at equator to zero - seems to work now

--- solarflux_v4.py
+++ solarflux_v4.py
@@ -27,7 +27,7 @@ start = time.time()
 a = 149597870.7*1000
 q = 1360
 
-latlist = [0.017, 0.17, 0.35, 0.52, 0.70, 0.87, 1.05, 1.22, 1.40, 1.56]
+latlist = [0, 0.17, 0.35, 0.52, 0.70, 0.87, 1.05, 1.22, 1.40, 1.56]
 for lat in latlist: #in radians
 
     sintheta = np.sin(lat)
