8d789a7666e38ee27b0e273f16585cca23da7647
quarkz24 <josephinehedley@outlook.com>
2023-10-10 16:50:57 +0100

    IR, albedo functions added

--- finite_difference.py
+++ finite_difference.py
@@ -19,18 +19,15 @@ start = time.time()
 q = 1360
 d = 5.394*100 #diffusivity, from 'habitable climates'
 a = 149597870.7*1000 
-c = 40 * 5.25e9 #heat cap, initially constant, of water planet
-#solar = 
-#albedo = 
+c = 40 * 5.25e9 #heat cap, initially constant, of water planet 
 del_t = 1
 del_lamb = 2 * (3.1415/180) #in degrees->radians
 initial_t = 300
-#outgoing = ? #maybe take this out for now
+sb = 5.670374419e-8
 
 ##arrays/lists
 
 latitude_list = np.linspace(0, 1.57, 45) #in radians
-#times_list = #is this necessary? last program just used range(0, 365, 1)
 temp_list = [300]*len(latitude_list) #this is the temperature at each of the 45 points on the Northern hemisphere
 
 ##for loops - finite difference method
@@ -45,6 +42,15 @@ solar = flux(latitude_list)
 
 for day in range(0, 365, 1):
     for i in range(len(latitude_list)): #does i start at zero?
+        
+        #IR cooling function
+        tir = 0.79*((temp_list[i]/273)**3)
+        outgoing = (sb * temp_list[i]**4)/(1+ (3/4)*tir)
+        
+        #albedo
+        albedo = 0.525 - 0.245*np.tanh((temp_list[i]-268)/5)
+        
+        #finite difference method
         new_temp = (2*del_t/c)*(solar[i][day]*(1-albedo) + d*((temp_list[i+1] - 2*temp_list[i] + temp_list[i-1])/del_lamb) - d*np.tan(latitude_list[i])*((temp_list[i+1] - temp_list[i-1])/2*del_lamb)) + temp_list[i-1] - outgoing
         temp_list[i] = new_temp
    
