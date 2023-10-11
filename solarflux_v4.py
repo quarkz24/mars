a4217072071c541c2ce0f59a062439879c21858e
quarkz24 <josephinehedley@outlook.com>
2023-10-11 12:40:18 +0100

    arccos fix
    same arccos fix as for flux function

--- solarflux_v4.py
+++ solarflux_v4.py
@@ -39,12 +39,14 @@ for lat in latlist: #in radians
         #delta remains in rad to feed into my trig functions
         
         delta = -0.409*np.cos((2*3.1415/365)*(day + 10))
-        big_h = np.arccos(-tantheta * np.tan(delta))
+        #big_h = np.arccos(-tantheta * np.tan(delta))
         
         if -tantheta*np.tan(delta) >1:
             big_h = np.arccos(1)
         elif -tantheta*np.tan(delta) <-1:
             big_h = np.arccos(-1)
+        else:
+            big_h = np.arccos(-tantheta * np.tan(delta))
         
         solar = (q/3.1415) * (((149597870.7*1000)/a)**2) * (big_h*sintheta*np.sin(delta) + costheta*np.cos(delta)*np.sin(big_h))
         flux.append(solar)
@@ -86,12 +88,14 @@ for lat in latlist2: #in radians
         #delta remains in rad to feed into my trig functions
             
         delta = -0.409*np.cos((2*3.1415/365)*(day + 10))
-        big_h = np.arccos(-tantheta * np.tan(delta))
+        #big_h = np.arccos(-tantheta * np.tan(delta))
         
         if -tantheta*np.tan(delta) >1:
             big_h = np.arccos(1)
         elif -tantheta*np.tan(delta) <-1:
             big_h = np.arccos(-1)
+        else:
+            big_h = np.arccos(-tantheta * np.tan(delta))
             
         solar = (q/3.1415) * (((149597870.7*1000)/a)**2) * (big_h*sintheta*np.sin(delta) + costheta*np.cos(delta)*np.sin(big_h))
         flux.append(solar)
