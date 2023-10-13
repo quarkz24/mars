a669d05a4fec0dfd4868bcb893d7b6db0cbeb9b0
quarkz24 <josephinehedley@outlook.com>
2023-10-13 13:37:17 +0100

    evolves now FIXED ISH
    fixed it was a fucking bracket.

--- finite_difference_v6.py
+++ finite_difference_v6.py
@@ -16,10 +16,10 @@ start = time.time()
 
 ##constants
 
-q = 1360
-d = 0.0001 #diffusivity, placeholder, inaccurate
-a = 0.5 #also false
-c = 40 * 5.25e9 #heat cap, initially constant, of water planet 
+q = 1360*24*60*60
+d = 0.001 #diffusivity, placeholder, inaccurate
+a = 0.1 #also false
+c = 4000 #heat cap, initially constant, of water planet 
 sb = 5.670374419e-8
 
 n = 144
@@ -33,30 +33,34 @@ final_time = 365
 temps = [initial_temp]*n #144 length list of initial temps
 lats = np.linspace(-1.57, 1.57, n) #both start and end inclusive
 solar = flux(lats) #returns a list of lists, indexable, returns list of flux over a year per lat
-
+##do i need a new list? a different method - feed in 300s, populate new list
 print("lats: ",len(lats))
 print("temps: ", len(temps))
 print("incoming flux: ", len(solar)) #144 lists of flux, for 144 latitudes
-
+print("incoming flux in south pole winter: ", solar[0][180]) #just verifying im getting an answer i expect
 
 ##looping
 ##only differential terms included so far - without outgoing or incoming flux, think this should have no change?
 
-for day in range(0, final_time, del_t): #gives list 0->364
-    for y in range(len(lats)-1): #0->143
-        temps[y+1] = (2*del_t/c)*(-d*np.tan(lats[y]))*((temps[y+1]-temps[y-1])/2*del_lamb) + d*((temps[y+1]-2*temps[y]+temps[y-1])/del_lamb**2) + temps[day-1]
-                                   
-    temps[len(lats)-1] = (2*del_t/c)*(-d*(temps[y] - temps[y-1])/(del_lamb**2)) + temps[day-1] # north pole
-    temps[0] = (2*del_t/c)*(d*((temps[y+1] - temps[y])/del_lamb**2)) + temps[day-1] #south pole - what is day-1 for first day?
-    ##maybe need to put time addition in time loop
+for day in range(0, final_time, del_t): #gives list 0->364, 365 entries
+    for y in range(len(lats)-1): #0->143, 144 entries
+        #ir cooling function
+        tir = 0.79*((temps[y]/273)**3)
+        ir = (sb*temps[y])/(1+0.75*tir)
+        
+        #when this eventually works and updates the list, heat cap might make the temps tiny? check units in paper
+        temps[y+1] = (2*del_t/c)*(solar[y][day]*(1-a) - d*np.tan(lats[y])*((temps[y+1]-temps[y-1])/2*del_lamb) + d*((temps[y+1]-2*temps[y] + temps[y-1])/(del_lamb**2)) - ir) + temps[day-1]
+    
+    temps[len(lats)-1] = (2*del_t/c)*(solar[y][day]*(1-a) - d*(temps[y] - temps[y-1])/(del_lamb**2)) + temps[day-1] # north pole
+    temps[0] = (2*del_t/c)*(solar[y][day]*(1-a) - d*((temps[y+1] - temps[y])/del_lamb**2)) + temps[day-1] #south pole - what is day-1 for first day?
     
     print("temps for day", day, ":", temps)
     
     plt.figure(1)
     plt.plot(lats, temps)
     plt.axis([-1.57, 1.57, 0, 700])
-    plt.xlabel('Distance (m), day')
-    plt.ylabel('Temperature (C)')
+    plt.xlabel(f'latitude, $day = {day}$') #label = f'$latitude = {lat}$'
+    plt.ylabel('Temps')
     plt.show()
     plt.pause(0.01) 
     
