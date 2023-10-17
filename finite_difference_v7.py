4d7ccdd426d22d0d3eea663c160963db222820ab
quarkz24 <josephinehedley@outlook.com>
2023-10-17 15:05:45 +0100

    fixed ir
    added some testing print statements. fixed outgoing ir. only plots every year now. still wont evolve

--- finite_difference_v7.py
+++ finite_difference_v7.py
@@ -40,37 +40,43 @@ temp_diff = np.empty(n)
 
 print("lats: ",len(lats))
 print("temps: ", len(temps))
-print("incoming flux: ", len(solar)) #144 lists of flux, for 144 latitudes
+#print("incoming flux: ", solar) #144 lists of flux, for 144 latitudes
 print("incoming flux in south pole winter: ", solar[0][180]) #just verifying im getting an answer i expect
 
 ##looping
 
 for day in range(0, final_time, del_t): #gives list 0->364, 365 entries
+    print("day: ", day)
     for y in range(len(lats)-1): #0->143, 144 entries
     
         #ir cooling function
         tir = 0.79*((temps[y]/273)**3)
-        ir = (sb*temps[y])/(1+0.75*tir)
+        ir = (sb*(temps[y])**4)/(1+0.75*tir)
         
         #albedo
         a = 0.525 - 0.245*np.tanh((temps[y]-268)/5)
         
+        print("flux in total: ", solar[y][day]*(1-a))
+        print("outgoing ir: ", ir)
+        
         temp_diff[y] = (2*del_t/c)*(solar[y][day]*(1-a) - d*np.tan(lats[y])*((temps[y+1]-temps[y-1])/2*del_lamb) + d*((temps[y+1]-2*temps[y] + temps[y-1])/(del_lamb**2)) - ir)
+        print("temp change for lat=", y, "is ", temp_diff[y]) #getting tiny temp changes! why?
     
     temp_diff[len(lats)-1] = (2*del_t/c)*(solar[y][day]*(1-a) - d*(temps[y] - temps[y-1])/(del_lamb**2) - ir) # north pole
     temp_diff[0] = (2*del_t/c)*(solar[y][day]*(1-a) - d*((temps[y+1] - temps[y])/del_lamb**2) - ir) #south pole - what is day-1 for first day?
     
     temps = temp_diff + temps
     
-    print("temps for day", day, ":", temps)
-    
-    #plt.figure(1)
-    #plt.plot(lats, temps)
-    #plt.axis([-1.57, 1.57, 0, 700])
-    #plt.xlabel(f'latitude, $day = {day}$') #label = f'$latitude = {lat}$'
-    #plt.ylabel('Temps')
-    #plt.show()
-    #plt.pause(0.01) 
+    #print("temps for day", day, ":", temps)
+    if day % 364 == 0:
+        
+        plt.figure(1)
+        plt.plot(lats, temps)
+        plt.axis([-1.57, 1.57, 0, 700])
+        plt.xlabel(f'latitude, $day = {day}$') #label = f'$latitude = {lat}$'
+        plt.ylabel('Temps')
+        plt.show()
+        plt.pause(0.001) 
     
 end = time.time()
 print("Time elapsed:", end - start)
